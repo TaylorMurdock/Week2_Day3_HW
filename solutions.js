a9242459fa0af4fe9c438dbaa983ec02ae974979
Taylor Murdock <tjmurd4545@gmail.com>
2023-05-09 11:08:04 -0400

    First commit

---
+++ solutions.js
@@ -0,0 +1,10 @@
+// setAlarm
+// Write a function named setAlarm that has two arguments.
+// The first argument: employed (the boolean true would be passed in as a parameter whenever you are employed)
+// The second argument: vacationing (the boolean true would be passed in as a parameter whenever you are on vacation)
+// The function should only return true if you are employed and not on vacation (because these are the circumstances under which you need to set an alarm, of course!). It should return false otherwise.
+// Example expected output:
+// setAlarm(true, true) // => returns false
+// setAlarm(false, false) // => returns false
+// setAlarm(false, true) // => returns false
+// setAlarm(true, false) // => returns true
