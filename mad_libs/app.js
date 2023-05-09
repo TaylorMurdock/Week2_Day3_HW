174e76055cb52dff9a7b948a946b2365262b21d3
Taylor Murdock <tjmurd4545@gmail.com>
2023-05-09 12:43:18 -0400

    second commit

---
+++ mad_libs/app.js
@@ -0,0 +1,8 @@
+// Building an Object
+// First, create an object called words with the keys given below. Each key's property should be an empty string. This is where we will save the user's input later.
+// The keys:
+// number
+// adjective
+// plural noun (pluralNoun)
+// adverb
+// another adjective (anotherAdjective)
